0c4ccf07991bdac093b4b367bbceee9a8925587e
vsobani <vishal.sobani@gmail.com>
2020-01-30 17:41:37 +0530

    A simple Post for CRUD operations

---
+++ crud_operations/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PostForm from './postform';
+import AllPost from './allpost';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+    
+        <PostForm/>
+        <AllPost/>
+      
+      </div>
+    );
+  }
+}
+
+export default App;
